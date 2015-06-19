925bbf2545aaafe7a39abfa7c987158745e5f814
Ilya Mikhaltsou <morpheby@gmail.com>
2015-06-19 17:25:46 +0300

    Added check for display

--- src/app_connector.c
+++ src/app_connector.c
@@ -1,1 +1,12 @@
 
+#include "app_connector.h"
+#include "board-config.h"
+
+
+/*
+ * Configuration checks
+ */
+#if !HAS_LED_DISPLAY && APP_USE_LED_DISP
+#error "Display module enabled, while no display module specified"
+#endif
+
