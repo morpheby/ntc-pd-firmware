c7b55faadae177e426416563a33a102426f398b0
Ilya Mikhaltsou <morpheby@gmail.com>
2015-07-29 17:50:02 +0300

    Added filtration and RMS

--- src/app_main.c
+++ src/app_main.c
@@ -16,6 +16,7 @@
 #include "modbus_registers.h"
 #include "float.h"
 #include <stdint.h>
+#include <math.h>
 
 #include "quadrature_encoder.h"
 #include  "menu-base.h"
@@ -48,14 +49,14 @@ MAIN_DECL_LOOP_FN() {
     MB.D_In = discrete_get_input();
     
     // Extract ADC values and push to modbus
-//    MB.A0 = filter_get(0);
-//    MB.A1 = filter_get(1);
-//    MB.A2 = filter_get(2);
-//    MB.A3 = filter_get(3);
-//    MB.A4 = filter_get(4);
-//    MB.A5 = filter_get(5);
-//    MB.A6 = filter_get(6);
-//    
+    MB.A0 = filter_get(adcInputFilter, 0);
+    MB.A1 = filter_get(adcInputFilter, 1);
+    MB.A2 = filter_get(adcInputFilter, 2);
+    MB.A3 = filter_get(adcInputFilter, 3);
+    MB.A4 = filter_get(adcInputFilter, 4);
+    MB.A5 = filter_get(adcInputFilter, 5);
+    MB.A6 = filter_get(adcInputFilter, 6);
+    
     // Process ADC values
     MB.ADC0 = (MB.A0 - MB.OFS_ADC0)*MB.K0;
     MB.ADC1 = (MB.A1 - MB.OFS_ADC1)*MB.K1;
@@ -64,37 +65,16 @@ MAIN_DECL_LOOP_FN() {
     MB.ADC4 = (MB.A4 - MB.OFS_ADC4)*MB.K4;
     MB.ADC5 = (MB.A5 - MB.OFS_ADC5)*MB.K5;
     MB.ADC6 = (MB.A6 - MB.OFS_ADC6)*MB.K6;
-        
-//    ADC4Sum += MB.ADC4;
-//    ADC5Sum += MB.ADC5;
-//    ADC6Sum += MB.ADC6;
-//    
-//    ADC4SumSq += (MB.ADC4*MB.ADC4);
-//    ADC5SumSq += (MB.ADC5*MB.ADC5);
-//    ADC6SumSq += (MB.ADC6*MB.ADC6);
-//    
-//    iterationsCount++;
-//    if(iterationsCount>MB.N)
-//    {
-//        MB.AD4_AVG = ADC4Sum / MB.N;
-//        MB.AD5_AVG = ADC5Sum / MB.N;
-//        MB.AD6_AVG = ADC6Sum / MB.N;
-//                
-//        MB.AD4_RMS = sqrt(ADC4SumSq / MB.N);
-//        MB.AD5_RMS = sqrt(ADC5SumSq / MB.N);
-//        MB.AD6_RMS = sqrt(ADC6SumSq / MB.N);
-//        
-//        iterationsCount = 0;
-//        ADC4Sum = 0;
-//        ADC5Sum = 0;
-//        ADC6Sum = 0;
-//        
-//        ADC4SumSq = 0;
-//        ADC5SumSq = 0;
-//        ADC6SumSq = 0;
-//    }
+    
+    filter_put(squaredFilter, 0, MB.A4);
+    filter_put(squaredFilter, 1, MB.A5);
+    filter_put(squaredFilter, 2, MB.A6);
+    
+    MB.AD4_RMS = sqrt((filter_get(squaredFilter, 0) - MB.OFS_ADC4)*MB.K4);
+    MB.AD5_RMS = sqrt((filter_get(squaredFilter, 1) - MB.OFS_ADC5)*MB.K5);
+    MB.AD6_RMS = sqrt((filter_get(squaredFilter, 2) - MB.OFS_ADC6)*MB.K6);
 }
 
 ADC_DECL_VALUE_FN(channel, value) {
-//    filter_put(channel, value);
+    filter_put(adcInputFilter, channel, value);
 }
