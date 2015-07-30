08698a286eb18c7a4518b541b1ec946f5b375666
Ilya Mikhaltsou <morpheby@gmail.com>
2015-07-30 15:41:12 +0300

    Fixup

--- src/app_main.c
+++ src/app_main.c
@@ -49,29 +49,29 @@ MAIN_DECL_LOOP_FN() {
     MB.D_In = discrete_get_input();
     
     // Extract ADC values and push to modbus
-    MB.A0 = filter_get(adcInputFilter, 0);
-    MB.A1 = filter_get(adcInputFilter, 1);
-    MB.A2 = filter_get(adcInputFilter, 2);
-    MB.A3 = filter_get(adcInputFilter, 3);
+    MB.A0 = filter_get(adcInputFilter, 0) - MB.OFS_ADC0;
+    MB.A1 = filter_get(adcInputFilter, 1) - MB.OFS_ADC1;
+    MB.A2 = filter_get(adcInputFilter, 2) - MB.OFS_ADC2;
+    MB.A3 = filter_get(adcInputFilter, 3) - MB.OFS_ADC3;
     MB.A4 = filter_get(adcInputFilter, 4) - MB.OFS_ADC4;
     MB.A5 = filter_get(adcInputFilter, 5) - MB.OFS_ADC5;
     MB.A6 = filter_get(adcInputFilter, 6) - MB.OFS_ADC6;
     
     // Process ADC values
-    MB.ADC0 = (MB.A0 - MB.OFS_ADC0)*MB.K0;
-    MB.ADC1 = (MB.A1 - MB.OFS_ADC1)*MB.K1;
-    MB.ADC2 = (MB.A2 - MB.OFS_ADC2)*MB.K2;
-    MB.ADC3 = (MB.A3 - MB.OFS_ADC3)*MB.K3;
+    MB.ADC0 = MB.A0 * MB.K0;
+    MB.ADC1 = MB.A1 * MB.K1;
+    MB.ADC2 = MB.A2 * MB.K2;
+    MB.ADC3 = MB.A3 * MB.K3;
     MB.ADC4 = MB.A4 * MB.K4;
     MB.ADC5 = MB.A5 * MB.K5;
     MB.ADC6 = MB.A6 * MB.K6;
     
-    filter_put(squaredFilter, MB.A4*MB.A4, 0);
-    filter_put(squaredFilter, MB.A5*MB.A5, 1);
-    filter_put(squaredFilter, MB.A6*MB.A6, 2);
-    MB.M0_RMS = sqrt(filter_get(squaredFilter, 0)) * MB.K4;
-    MB.M1_RMS = sqrt(filter_get(squaredFilter, 1)) * MB.K5;
-    MB.M2_RMS = sqrt(filter_get(squaredFilter, 2)) * MB.K6;
+    filter_put(squaredFilter, MB.A0*MB.A0, 0);
+    filter_put(squaredFilter, MB.A1*MB.A1, 1);
+    filter_put(squaredFilter, MB.A2*MB.A2, 2);
+    MB.M0_RMS = sqrt(filter_get(squaredFilter, 0)) * MB.K0;
+    MB.M1_RMS = sqrt(filter_get(squaredFilter, 1)) * MB.K1;
+    MB.M2_RMS = sqrt(filter_get(squaredFilter, 2)) * MB.K2;
 }
 
 ADC_DECL_VALUE_FN(channel, value) {
