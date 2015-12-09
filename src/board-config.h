d8cd71da8a4af9fd425ae87ce6f4f57cf3421af8
Evgeny Kravchenko <evgeny.kravchenko1@gmail.com>
2015-12-09 09:13:55 +0300

    NTC-13.09.4 Enable DI2 DI3 impuls counters

--- src/board-config.h
+++ src/board-config.h
@@ -97,8 +97,8 @@
 
 #define COUNT_DI0_IMP_FREQUENCY 0
 #define COUNT_DI1_IMP_FREQUENCY 0
-#define COUNT_DI2_IMP_FREQUENCY 0
-#define COUNT_DI3_IMP_FREQUENCY 0
+#define COUNT_DI2_IMP_FREQUENCY 1
+#define COUNT_DI3_IMP_FREQUENCY 1
 /*
  * Change notification pins
  * Set ENABLE to 1 to enable interrupts on the pin. Numbering is the same, as in
