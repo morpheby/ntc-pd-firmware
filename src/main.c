2f216154d3b3df938d00a2ff2a3e82b9aa96bdf8
Evgeny Kravchenko <evgeny.kravchenko1@gmail.com>
2015-12-25 17:28:30 +0300

    Default prof change source

--- src/main.c
+++ src/main.c
@@ -47,7 +47,7 @@ int _FLASH_STORE _FLASH_ACCESS flash_data_buf_IND_PROFILES[13] = {1, //current p
                                                                      DEFAULT_IND_PROFILE,
                                                                      DEFAULT_IND_PROFILE, 
                                                                      DEFAULT_IND_PROFILE};
-unsigned int _FLASH_STORE _FLASH_ACCESS flash_data_buf_PROF_CHANGE_SOURCE = 0;
+unsigned int _FLASH_STORE _FLASH_ACCESS flash_data_buf_PROF_CHANGE_SOURCE = 1;
 unsigned int _FLASH_STORE _FLASH_ACCESS flash_data_buf_Ind_Delay = 0;
 unsigned int _FLASH_STORE _FLASH_ACCESS flash_data_buf_D_Out_Init = 0;
 
